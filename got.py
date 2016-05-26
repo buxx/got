7581ecd45182f10289ad31e21bd6571d6d728ef2
Bastien Sevajol <sevajol.bastien@gmail.com>
2016-05-26 07:29:30 +0200

    links

--- got.py
+++ got.py
@@ -6,5 +6,6 @@ search = "Game.of.Thrones.S06E05"
 r = requests.get('http://www.t411.ch/torrents/search/?name=%22Game.of.Thrones.S06E05%22&description=&file=&user=&cat=&search=%40name+%22Game.of.Thrones.S06E05%22+&submit=Recherche&order=added&type=desc')
 d = pq(r.text)
 g = d('.results a[title^="{0}"]'.format(search))
+urls = [l.attrib['href'] for l in g]
 
-print(g)
+print(urls)
